3bd8716bb2eb2961042172056667488883dc5c36
Mariano Custiel <jmcustiel@gmail.com>
2023-08-02 15:09:28 +0200

    chore: some fixes

--- .config/nvim/lua/custom/plugins/nvim-tree.lua
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -1,10 +1,12 @@
 return {
   "nvim-tree/nvim-tree.lua",
+
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
+
   config = function()
     require("nvim-tree").setup({
       sort_by = "case_sensitive",
--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -21,17 +21,17 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
-    "mxsdev/nvim-dap-vscode-js",
+    'mxsdev/nvim-dap-vscode-js',
 
     {
-      "microsoft/vscode-js-debug",
+      'microsoft/vscode-js-debug',
       config = function()
       end,
-      -- ft = {"typescript", "javascript"},
+      -- ft = {'typescript', 'javascript'},
       -- opt = true,
-      -- build = "npm install --legacy-peer-deps && npm run compile",
+      -- build = 'npm install --legacy-peer-deps && npm run compile',
       build =
-      "rm -rf out && npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out && git checkout -- package*.json",
+      'rm -rf out && npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out && git checkout -- package*.json',
     },
 
   },
@@ -111,13 +111,13 @@ return {
     -- Install golang specific config
     -- require('dap-go').setup()
 
-    require("dap-vscode-js").setup({
-      -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-      debugger_path = os.getenv("MC_JS_DEBUG") or vim.fn.stdpath('data') .. "/lazy/vscode-js-debug", -- Path to vscode-js-debug installation.
-      -- debugger_cmd = { "js-debug-adapter" },                             -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+    require('dap-vscode-js').setup({
+      -- node_path = 'node', -- Path of node executable. Defaults to $NODE_PATH, and then 'node'
+      debugger_path = os.getenv('MC_JS_DEBUG') or vim.fn.stdpath('data') .. '/lazy/vscode-js-debug', -- Path to vscode-js-debug installation.
+      -- debugger_cmd = { 'js-debug-adapter' },                             -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
       adapters = { 'pwa-node', 'pwa-chrome', 'node-terminal' },             -- which adapters to register in nvim-dap
 
-      -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+      -- log_file_path = '(stdpath cache)/dap_vscode_js.log' -- Path for file logging
       -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
       -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
     })
