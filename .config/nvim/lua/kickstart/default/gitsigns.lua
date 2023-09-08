649ae12b1d88848ed3a0195c37021523f9cb29a1
Mariano Custiel <jmcustiel@gmail.com>
2023-09-08 07:40:02 +0200

    style: formatting

--- .config/nvim/lua/kickstart/default/gitsigns.lua
+++ .config/nvim/lua/kickstart/default/gitsigns.lua
@@ -11,6 +11,7 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+
     on_attach = function(bufnr)
       vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
 
